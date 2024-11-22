9bcd81d27cce0d67481cf3295001e21c90e3feb3
AnugunjNaman <anugunjjha@gmail.com>
2024-11-22 17:19:56 -0500

    aligned the website for table

--- workorderapp/src/ConfigPane.tsx
+++ workorderapp/src/ConfigPane.tsx
@@ -83,158 +83,163 @@ export const ConfigPane = observer(function ConfigPane() {
 
 
     return (
-        <div style={{ width: '30vw', height: '90vh', padding: '5px' }} >
+        <div style={{ width: '48vw', height: '90vh', padding: '3px', overflow: 'auto' }}>
 
-            {!window.location.toString().match(/debug/) ? <React.Fragment /> :
-                <Autocomplete
-                    style={{ marginTop: '10px', marginBottom: '5px' }}
-                    options={state.geojsonviz.files}
-                    value={state.geojsonviz.selectedFile}
-                    onChange={(_evt, value) => actions.selectGeojsonVizFile(value as string)}
-                    renderInput={(params) => <TextField {...params} label="Load Road Tile" />}
-                />
-            }
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
 
-            <div style={{ padding: '5px', margin: '5px', height: '12%', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #000088', borderRadius: '3px' }}
-                onDragOver={handleFile({ filetype: 'tracks', eventtype: 'drag' })}
-                onDrop={handleFile({ filetype: 'tracks', eventtype: 'drop' })}
-                onDragEnter={handleFile({ filetype: 'tracks', eventtype: 'drag', inout: true })}
-                onDragLeave={handleFile({ filetype: 'tracks', eventtype: 'drag', inout: false })}
-            >
-                {
-                    !state.parsing.inprogress && !state.daytracks.rev ? 'Drop GPS tracks file here.' :
-                        <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                            {state.parsing.state === 'roads'
-                                ? <div>Identifying roads: {numeral(numrows).format('0,0')} Points</div>
-                                : state.parsing.state === 'preprocessed'
-                                    ? <div>Loading preprocessd tracks...</div>
-                                    : <div>Loaded {numeral(numrows).format('0,0')} Points ({state.parsing.state})</div>
-                            }
-
-                            {state.parsing.state !== 'preprocessed'
-                                ? <div style={{ flexGrow: 1, width: '100%' }}>
-                                    <LinearProgress variant="determinate" value={100 * numrows / (state.parsing.estimatedRows || 1)} />
-                                </div>
-                                : <React.Fragment />
-                            }
-
-                            {state.parsing.inprogress ? <React.Fragment /> :
-                                <Button onClick={() => actions.exportProcessedTracks()}>Export Processed Tracks</Button>
-                            }
-                        </div>
-                }
-            </div>
-
-
-
-            <div style={{ padding: '5px', margin: '5px', height: '12%', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #008800', borderRadius: '3px' }}
-                onDragOver={handleFile({ filetype: 'workorders', eventtype: 'drag' })}
-                onDrop={handleFile({ filetype: 'workorders', eventtype: 'drop' })}
-                onDragEnter={handleFile({ filetype: 'workorders', eventtype: 'drag', inout: true })}
-                onDragLeave={handleFile({ filetype: 'workorders', eventtype: 'drag', inout: false })}
-            >
-                {
-                    state.knownWorkorders.parsing
-                        ? 'Reading work orders...'
-                        : !state.knownWorkorders.orders.rev
-                            ? 'Drop work orders spreadsheet here to validate.'
-                            : <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                                <div>Loaded {numeral(actions.numKnownWorkorders()).format('0,0')} Work Orders</div>
-                            </div>
-                }
-            </div>
-
-            <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                <Button
-                    style={{ flexGrow: 1 }}
-                    onClick={() => actions.validateWorkorders()}
-                    variant="contained"
-                    disabled={!actions.knownWorkorders() || !actions.daytracks()}
+                <div style={{ margin: '5px', width: '16vw', height: '10vh', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #000088', borderRadius: '3px' }}
+                    onDragOver={handleFile({ filetype: 'tracks', eventtype: 'drag' })}
+                    onDrop={handleFile({ filetype: 'tracks', eventtype: 'drop' })}
+                    onDragEnter={handleFile({ filetype: 'tracks', eventtype: 'drag', inout: true })}
+                    onDragLeave={handleFile({ filetype: 'tracks', eventtype: 'drag', inout: false })}
                 >
-                    Validate Work Orders
-                </Button>
-            </div>
+                    {
+                        !state.parsing.inprogress && !state.daytracks.rev ? 'Drop GPS tracks.' :
+                            <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                {state.parsing.state === 'roads'
+                                    ? <div>Identifying roads: {numeral(numrows).format('0,0')} Points</div>
+                                    : state.parsing.state === 'preprocessed'
+                                        ? <div>Loading preprocessd tracks...</div>
+                                        : <div>Loaded {numeral(numrows).format('0,0')} Points ({state.parsing.state})</div>
+                                }
 
-            <div style={{ padding: '10px', margin: '5px', height: '20%', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #008800', borderRadius: '3px' }}
-                onDragOver={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag' })}
-                onDrop={handleFile({ filetype: 'vehicleactivities', eventtype: 'drop' })}
-                onDragEnter={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag', inout: true })}
-                onDragLeave={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag', inout: false })}
-            >
-                {
-                    state.createdWorkOrders.parsing
-                        ? 'Reading vehicle activities...'
-                        : !state.createdWorkOrders.vehicleActivities.rev
-                            ? 'Drop vehicle activities spreadsheet here.'
-                            : <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                                <div>Found {numeral((actions.vehicleActivities() || []).length).format('0,0')} Vehicle Activities</div>
-                                {state.createdWorkOrders.workorders.rev > 0
-                                    ? <div>Successfully created {numeral(actions.createdWorkOrders()?.length || 0).format('0,0')} Work Orders</div>
+                                {state.parsing.state !== 'preprocessed'
+                                    ? <div style={{ flexGrow: 1, width: '100%' }}>
+                                        <LinearProgress variant="determinate" value={100 * numrows / (state.parsing.estimatedRows || 1)} />
+                                    </div>
                                     : <React.Fragment />
                                 }
+
+                                {state.parsing.inprogress ? <React.Fragment /> :
+                                    <Button onClick={() => actions.exportProcessedTracks()}>Export Processed Tracks</Button>
+                                }
                             </div>
-                }
+                    }
+                </div>
+
+                <div style={{ margin: '5px', width: '16vw', height: '10vh', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #008800', borderRadius: '3px' }}
+                    onDragOver={handleFile({ filetype: 'workorders', eventtype: 'drag' })}
+                    onDrop={handleFile({ filetype: 'workorders', eventtype: 'drop' })}
+                    onDragEnter={handleFile({ filetype: 'workorders', eventtype: 'drag', inout: true })}
+                    onDragLeave={handleFile({ filetype: 'workorders', eventtype: 'drag', inout: false })}
+                >
+                    {
+                        state.knownWorkorders.parsing
+                            ? 'Reading work orders...'
+                            : !state.knownWorkorders.orders.rev
+                                ? 'Drop work orders.'
+                                : <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                    <div>Loaded {numeral(actions.numKnownWorkorders()).format('0,0')} Work Orders</div>
+                                </div>
+                    }
+                </div>
+
+                <div style={{ margin: '5px', width: '16vw', height: '10vh', alignItems: 'center', justifyContent: 'center', display: 'flex', border: '3px dashed #008800', borderRadius: '3px' }}
+                    onDragOver={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag' })}
+                    onDrop={handleFile({ filetype: 'vehicleactivities', eventtype: 'drop' })}
+                    onDragEnter={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag', inout: true })}
+                    onDragLeave={handleFile({ filetype: 'vehicleactivities', eventtype: 'drag', inout: false })}
+                >
+                    {
+                        state.createdWorkOrders.parsing
+                            ? 'Reading vehicle activities...'
+                            : !state.createdWorkOrders.vehicleActivities.rev
+                                ? 'Drop vehicle activities.'
+                                : <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                    <div>Found {numeral((actions.vehicleActivities() || []).length).format('0,0')} Vehicle Activities</div>
+                                    {state.createdWorkOrders.workorders.rev > 0
+                                        ? <div>Successfully created {numeral(actions.createdWorkOrders()?.length || 0).format('0,0')} Work Orders</div>
+                                        : <React.Fragment />
+                                    }
+                                </div>
+                    }
+                </div>
+
             </div>
 
-            <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                <Button
-                    style={{ flexGrow: 1 }}
-                    onClick={() => {
-                        if (state.createdWorkOrders.parsing) return;
-                        actions.createWorkOrders()
-                    }}
-                    variant="contained"
-                    disabled={!actions.vehicleActivities() || !actions.daytracks() || state.createdWorkOrders.parsing || state.createdWorkOrders.workorders.rev > 0}
-                >
-                    Create Work Records from GPS Tracks (PoC)
-                </Button>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }} >
+
+                <div style={{ margin: '5px', width: '25vw', height: '10vh', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                    <Button
+                        style={{ flexGrow: 1 }}
+                        onClick={() => actions.validateWorkorders()}
+                        variant="contained"
+                        disabled={!actions.knownWorkorders() || !actions.daytracks()}
+                    >
+                        Validate Work Orders
+                    </Button>
+                </div>
+
+                <div style={{ margin: '5px', width: '25vw', height: '10vh', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                    <Button
+                        style={{ flexGrow: 1 }}
+                        onClick={() => {
+                            if (state.createdWorkOrders.parsing) return;
+                            actions.createWorkOrders()
+                        }}
+                        variant="contained"
+                        disabled={!actions.vehicleActivities() || !actions.daytracks() || state.createdWorkOrders.parsing || state.createdWorkOrders.workorders.rev > 0}
+                    >
+                        Create Work Records
+                    </Button>
+                </div>
+
             </div>
 
             <div style={{ padding: '5px' }}>
-                <div style={{ padding: '5px' }}>
-                    <Slider
-                        value={state.sliderValue}
-                        onChange={handleSliderChange}
-                        aria-labelledby="input-slider"
-                        min={0.001}
-                        max={1}
-                        step={0.001}
-                    />
-                </div>
+                <Slider
+                    value={state.sliderValue}
+                    onChange={handleSliderChange}
+                    aria-labelledby="input-slider"
+                    min={0.001}
+                    max={1}
+                    step={0.001}
+                />
+            </div>
 
-                <div style={{ padding: '5px' }}>
-                    <Select
-                        value={selectedDate}
-                        onChange={handleChangeDate}
-                        displayEmpty
-                        style={{ marginRight: '50px', marginLeft: '30px' }} // Add spacing between the two Select components
-                    >
-                        <MenuItem value="" disabled>Select Date</MenuItem>
-                        {actions.getDateList().map(date => (
-                            <MenuItem key={date} value={date}>
-                                {date}
-                            </MenuItem>
-                        ))}
-                    </Select>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+                <Select
+                    value={selectedDate}
+                    onChange={handleChangeDate}
+                    displayEmpty
+                    style={{
+                        minWidth: '150px', // Set a consistent width for the Select component
+                        fontSize: '14px', // Use a smaller font size for compactness
+                        height: '30px', // Set a reduced height
+                        lineHeight: '30px', // Align text vertically
+                        padding: '0 10px', // Reduce padding for a thinner look
+                    }}
+                >
+                    <MenuItem value="" disabled>Select Date</MenuItem>
+                    {actions.getDateList().map(date => (
+                        <MenuItem key={date} value={date}>
+                            {date}
+                        </MenuItem>
+                    ))}
+                </Select>
 
-                    <Select
-                        value={selectedVehicle}
-                        onChange={handleChangeVehicle}
-                        displayEmpty
-                        style={{ marginRight: '10px' }} // Add spacing between the two Select components
-                    >
-                        <MenuItem value="" disabled>Select Vehicle</MenuItem>
-                        {vehicleList.map(vehicle => (
-                            <MenuItem key={vehicle.vehicleId} value={vehicle.vehicleId}>
-                                {vehicle.vehicleId} ({numeral(vehicle.count).format('0,0')} points)
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </div>
+                <Select
+                    value={selectedVehicle}
+                    onChange={handleChangeVehicle}
+                    displayEmpty
+                    style={{
+                        minWidth: '150px', // Set a consistent width for the Select component
+                        fontSize: '14px', // Use a smaller font size for compactness
+                        height: '30px', // Set a reduced height
+                        lineHeight: '30px', // Align text vertically
+                        padding: '0 10px', // Reduce padding for a thinner look
+                    }}
+                >
+                    <MenuItem value="" disabled>Select Vehicle</MenuItem>
+                    {vehicleList.map(vehicle => (
+                        <MenuItem key={vehicle.vehicleId} value={vehicle.vehicleId}>
+                            {vehicle.vehicleId} ({numeral(vehicle.count).format('0,0')} points)
+                        </MenuItem>
+                    ))}
+                </Select>
             </div>
         </div>
     );
-
 });
 
 
--- workorderapp/src/Map.tsx
+++ workorderapp/src/Map.tsx
@@ -11,130 +11,131 @@ const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXVsdGFjIiwiYSI6ImNsMXA4MzU3NTAzbzUzZW55ajhiM2F
 
 
 export let mapRef: React.MutableRefObject<MapRef | undefined> | null = null;
+
 export const Map = observer(function Map() {
-  mapRef = React.useRef<MapRef>()!;
-  const { state, actions } = React.useContext(context);
-  //-------------------------------------------------------------------
-  // Filter any roads/milemarkers if available:
-  // Access the rev so we are updated when it changes.  Have to access it BEFORE !geojson or it might not re-render
-  let roads: FeatureCollection | null = actions.roads() as FeatureCollection;
-  let milemarkers: GeoJSON | null = actions.milemarkers();
+    mapRef = React.useRef<MapRef>()!;
+    const { state, actions } = React.useContext(context);
+    //-------------------------------------------------------------------
+    // Filter any roads/milemarkers if available:
+    // Access the rev so we are updated when it changes.  Have to access it BEFORE !geojson or it might not re-render
+    let roads: FeatureCollection | null = actions.roads() as FeatureCollection;
+    let milemarkers: GeoJSON | null = actions.milemarkers();
 
-  if (state.roads.rev < 1 || !roads) {
-    roads = null;
-  }
-  if (state.milemarkers.rev < 1 || !milemarkers) {
-    milemarkers = null;
-  }
-  // filter features to include only those that match the search:
-  if (roads && state.search) {
-    roads = {
-      ...roads,
-      features: roads.features.filter(f => JSON.stringify(f.properties).match(state.search)),
-    };
-  }
+    if (state.roads.rev < 1 || !roads) {
+        roads = null;
+    }
+    if (state.milemarkers.rev < 1 || !milemarkers) {
+        milemarkers = null;
+    }
+    // filter features to include only those that match the search:
+    if (roads && state.search) {
+        roads = {
+            ...roads,
+            features: roads.features.filter(f => JSON.stringify(f.properties).match(state.search)),
+        };
+    }
 
-  //-------------------------------------------------------------
-  // show tracks if loaded
-  let tracks: FeatureCollection | null = actions.filteredGeoJSON();
-  if (state.filteredGeoJSON.rev < 1 || !tracks) {
-    tracks = null;
-  }
+    //-------------------------------------------------------------
+    // show tracks if loaded
+    let tracks: FeatureCollection | null = actions.filteredGeoJSON();
+    if (state.filteredGeoJSON.rev < 1 || !tracks) {
+        tracks = null;
+    }
 
-  //------------------------------------------------------------
-  // Mouse Events:
-  const onHover = React.useCallback((evt: MapLayerMouseEvent) => {
-    const active = evt.features && evt.features.length > 0 || false;
-    actions.hover({
-      x: evt.point.x,
-      y: evt.point.y,
-      lat: evt.lngLat.lat,
-      lon: evt.lngLat.lng,
-      features: (((evt.features as unknown) || []) as any[]),
-      active,
-    });
-  }, []);
+    //------------------------------------------------------------
+    // Mouse Events:
+    const onHover = React.useCallback((evt: MapLayerMouseEvent) => {
+        const active = evt.features && evt.features.length > 0 || false;
+        actions.hover({
+            x: evt.point.x,
+            y: evt.point.y,
+            lat: evt.lngLat.lat,
+            lon: evt.lngLat.lng,
+            features: (((evt.features as unknown) || []) as any[]),
+            active,
+        });
+    }, []);
 
-  const onLeave = () => {
-    actions.hover({ x: 0, y: 0, lat: 0, lon: 0, features: [], active: false });
-  }
+    const onLeave = () => {
+        actions.hover({ x: 0, y: 0, lat: 0, lon: 0, features: [], active: false });
+    }
 
-  const onClick = async (evt: MapLayerMouseEvent) => {
-    await navigator.clipboard.writeText(`{ lon: ${evt.lngLat.lng}, lat: ${evt.lngLat.lat} }`);
-  }
+    const onClick = async (evt: MapLayerMouseEvent) => {
+        await navigator.clipboard.writeText(`{ lon: ${evt.lngLat.lng}, lat: ${evt.lngLat.lat} }`);
+    }
 
-  const interactiveLayerIds = [];
-  if (roads) interactiveLayerIds.push('roads');
-  if (milemarkers) interactiveLayerIds.push('milemarkers');
-  
-  return (
-    <ReactMapGl
-      mapboxAccessToken={MAPBOX_TOKEN}
-      initialViewState={state.viewport}
-      style={{ width: '70vw', height: '90vh' }}
-      mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
-      onClick={onClick}
-      onMouseMove={onHover}
-      onMouseLeave={onLeave}
-      interactiveLayerIds={interactiveLayerIds}
-    >
+    const interactiveLayerIds = [];
+    if (roads) interactiveLayerIds.push('roads');
+    if (milemarkers) interactiveLayerIds.push('milemarkers');
 
-      {!roads ? <React.Fragment /> :
-        <Source type="geojson" data={roads as any}>
-          <Layer id="roads" type="line" paint={{
-            'line-color': '#FF0000',
-            'line-width': 2,
-          }} />
-        </Source>
-      }
+    return (
+        <ReactMapGl
+            mapboxAccessToken={MAPBOX_TOKEN}
+            initialViewState={state.viewport}
+            style={{ width: '52vw', height: '90vh' }}
+            mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+            onClick={onClick}
+            onMouseMove={onHover}
+            onMouseLeave={onLeave}
+            interactiveLayerIds={interactiveLayerIds}
+        >
 
-      <MapHoverInfo />
+            {!roads ? <React.Fragment /> :
+                <Source type="geojson" data={roads as any}>
+                    <Layer id="roads" type="line" paint={{
+                        'line-color': '#FF0000',
+                        'line-width': 2,
+                    }} />
+                </Source>
+            }
 
-      {!milemarkers ? <React.Fragment /> :
-        <Source type="geojson" data={milemarkers as any}>
-          <Layer id="milemarkers" type="circle" paint={{
-            'circle-radius': 2,
-            'circle-color': '#FF00FF',
-            'circle-stroke-width': 1,
-          }} />
-        </Source>
-      }
+            <MapHoverInfo />
 
-      {
-        !tracks ?  <React.Fragment />
-        : (
-          <Source type="geojson" data={tracks as any} lineMetrics={true}>
-            <Layer
-              id="tracks"
-              type="line"
-              paint={{
-                'line-color': 'red',
-                'line-width': [
-                  'interpolate',
-                  ['linear'],
-                  ['line-progress'],
-                  0,
-                  5
-                ],
-                'line-gradient': [
-                  'interpolate',
-                  ['linear'],
-                  ['line-progress'],
-                  0,
-                  'red', // Start at red
-                  state.sliderValue,
-                  'blue', // Continue with red until the slider value
-                  state.sliderValue + 0.01,
-                  'rgba(0, 0, 0, 0)', // Transition to transparent immediately after slider value
-                ],
-              }}
-            />
-          </Source>
-        )
-      }
+            {!milemarkers ? <React.Fragment /> :
+                <Source type="geojson" data={milemarkers as any}>
+                    <Layer id="milemarkers" type="circle" paint={{
+                        'circle-radius': 2,
+                        'circle-color': '#FF00FF',
+                        'circle-stroke-width': 1,
+                    }} />
+                </Source>
+            }
 
-    </ReactMapGl>
-  );
+            {
+                !tracks ? <React.Fragment />
+                    : (
+                        <Source type="geojson" data={tracks as any} lineMetrics={true}>
+                            <Layer
+                                id="tracks"
+                                type="line"
+                                paint={{
+                                    'line-color': 'red',
+                                    'line-width': [
+                                        'interpolate',
+                                        ['linear'],
+                                        ['line-progress'],
+                                        0,
+                                        5
+                                    ],
+                                    'line-gradient': [
+                                        'interpolate',
+                                        ['linear'],
+                                        ['line-progress'],
+                                        0,
+                                        'red', // Start at red
+                                        state.sliderValue,
+                                        'blue', // Continue with red until the slider value
+                                        state.sliderValue + 0.01,
+                                        'rgba(0, 0, 0, 0)', // Transition to transparent immediately after slider value
+                                    ],
+                                }}
+                            />
+                        </Source>
+                    )
+            }
+
+        </ReactMapGl>
+    );
 });
 
 
